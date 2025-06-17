c55b555bbe9cae3754069c1d103761dbe0731f8c
Radhikasharma723 <radhikasharma9201@gmail.com>
2025-06-17 12:46:32 +0530

    first codes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # retail-sales-dashboard
-hello
+![
+    
+](<Screenshot 2025-06-17 123140.png>)
+![
+](<Screenshot 2025-06-17 123223.png>)
+![
+](<Screenshot 2025-06-17 123328.png>)
+![
+](<Screenshot 2025-06-17 123459.png>)
+![
+](<Screenshot 2025-06-17 123533.png>)
--- project.py
+++ project.py
@@ -16,20 +16,20 @@ print(df.to_csv)
 
 #Product Category
  # Sales by Region
-ax = sns.barplot(data=df, x='Region', y='Sales', estimator=sum)
+'''ax = sns.barplot(data=df, x='Region', y='Sales', estimator=sum)
 
 # Add labels on top of bars
 for container in ax.containers:
     ax.bar_label(container)
 
 plt.title("Total Sales by Region")
-plt.show()
+plt.show()'''
 
 
 
 # total number of orders from top 10 city
 # Step 1: Group by City and count orders
-top_cities = df.groupby('City').size().reset_index(name='Orders')
+'''top_cities = df.groupby('City').size().reset_index(name='Orders')
 
 # Step 2: Sort and take top 10
 top_cities = top_cities.sort_values(by='Orders', ascending=False).head(10)
@@ -40,11 +40,11 @@ sns.barplot(data=top_cities, x='City', y='Orders')
 plt.title("Total Number of Orders from Top 10 Cities")
 plt.xticks(rotation=45)
 plt.tight_layout()
-plt.show()
+plt.show()'''
 
 # total number of orders from top 10 states
 # Step 1: Group by State and count orders
-top_states = df.groupby('State').size().reset_index(name='Orders')
+'''top_states = df.groupby('State').size().reset_index(name='Orders')
 
 # Step 2: Sort and take top 10
 top_states = top_states.sort_values(by='Orders', ascending=False).head(10)
@@ -55,12 +55,12 @@ sns.barplot(data=top_states, x='State', y='Orders')
 plt.title("Total Number of Orders from Top 10 States")
 plt.xticks(rotation=45)
 plt.tight_layout()
-plt.show()
+plt.show()'''
 
 
 #Top Category
 # Step 1: Group by Category and count orders
-top_category = df.groupby('Category').size().reset_index(name='Orders')
+'''top_category = df.groupby('Category').size().reset_index(name='Orders')
 
 # Step 2: Sort (optional here, since there are usually few categories)
 top_category = top_category.sort_values(by='Orders', ascending=False)
@@ -71,7 +71,7 @@ sns.barplot(data=top_category, x='Category', y='Orders')
 plt.title("Total Number of Orders by Category")
 plt.xticks(rotation=45)
 plt.tight_layout()
-plt.show()
+plt.show()'''
 
 #top 10 Sub-Categories
 # Step 1: Group by Sub-Category and count orders
